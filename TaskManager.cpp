cd9257457b5fb9d24175a90dc39eaca1239ee2f7
Rodion <rodion.shyshkin@teamdev.com>
2020-07-15 17:57:49 +0300

    Adding TaskManager & functions addTask & showAllTasks.

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -8,22 +8,32 @@ TaskManager::TaskManager() = default;
 TaskManager::~TaskManager() = default;
 
 void TaskManager::showAllTasks() const {
-  for(auto item : tasks) {
-//    std::cout << *item << std::endl;
+  /*for(auto item : tasks) {
     item->showTask();
   }
-  if(tasks.empty()) std::cout << "Vector is empty" << std::endl;
-
-  for(auto [key, value] : sortedTasks) {
-    value->showTask();
+  if(tasks.empty()) std::cout << "Vector is empty" << std::endl;*/
+  if(sortedTasks.empty()) { std::cout << "No tasks" << std::endl; }
+  else {
+    for (auto[key, value] : sortedTasks) {
+      value->showTask();
+    }
   }
 }
 
+/*void TaskManager::showTasksForToday() const {
+  int today = 99;
+  if(sortedTasks.empty()) { std::cout << "no tasks" << std::endl; }
+  else {
+    std::vector<Task> todayTasks;
+    for(auto[priority, task] : sortedTasks) {
+      if(task->getDate() == today) {
+        todayTasks.push_back(task);
+      }
+    }
+  }
+}*/
+
 void TaskManager::addTask(std::shared_ptr<Task> task) {
-  Priority temp = task->getPriority();
-  std::cout << temp << std::endl;
   tasks.push_back(task);
-
-
   sortedTasks.insert(std::pair<Priority, std::shared_ptr<Task>>(task->getPriority(), task));
 }
