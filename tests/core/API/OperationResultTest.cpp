325b0e8c39e2cba4038eadd86a6c7a6cd9fddf84
Rodion <rodion.shyshkin@teamdev.com>
2020-10-06 22:33:10 +0300

    Update OperationResult test

---
+++ tests/core/API/OperationResultTest.cpp
@@ -0,0 +1,22 @@
+//
+// Created by rodion on 8/10/20.
+//
+
+#include <gtest/gtest.h>
+#include <API/OperationResult/OperationResult.h>
+#include <src/core/API/OperationResult/StorageError.h>
+#include <src/core/API/OperationResult/PersistError.h>
+
+class OperationResultTest : public ::testing::Test {
+
+};
+
+TEST_F(OperationResultTest, ConstructorAndGetters) {
+  auto first_result = OperationResult<StorageError>::Fail(StorageError::INVALID_TASK);
+  auto second_result = OperationResult<PersistError>::Fail(PersistError::SERIALIZATION_ERROR);
+  auto third_result = OperationResult<StorageError>::Success();
+
+  ASSERT_EQ(first_result.GetError(), StorageError::INVALID_TASK);
+  ASSERT_EQ(second_result.GetError(), PersistError::SERIALIZATION_ERROR);
+  ASSERT_EQ(third_result.GetError(), std::nullopt);
+}
