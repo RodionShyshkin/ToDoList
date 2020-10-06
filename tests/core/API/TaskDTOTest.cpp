d57cc8466dfcbdadb3994630b93844123d76c8be
Rodion <rodion.shyshkin@teamdev.com>
2020-10-06 14:02:37 +0300

    ModelTaskDTO tested.

--- tests/core/API/TaskDTOTest.cpp
+++ tests/core/API/TaskDTOTest.cpp
@@ -9,33 +9,26 @@
 class TaskDTOTest : public ::testing::Test {
  public:
   void SetUp() override {
-    validTask = Task::create("Name", "Valid task", Priority::EMPTY, Date{"2090-1-1"}).value();
+
   }
 
   void TearDown() override {
 
   }
-
-  Task validTask;
 };
 
-TEST_F(TaskDTOTest, ExceptionWithInvalidTask) {
-  EXPECT_NO_THROW(TaskDTO::create(900, "", "invalid task", Priority::EMPTY, boost::gregorian::date{2010-10-10}, true));
-}
-
 TEST_F(TaskDTOTest, validTask) {
-  TaskDTO dto;
-  EXPECT_NO_THROW(dto = TaskDTO::create(5, validTask.GetName(), validTask.GetLabel(),
-                                        validTask.GetPriority(), validTask.GetDate().GetDate(), false));
-  EXPECT_NO_THROW(TaskDTO::create(900,
-                                   "Invalid", "", Priority::EMPTY,
-                                                boost::gregorian::date{2020-9-8}, true));
+  unsigned int id = 4;
+  boost::gregorian::date date{2020-10-6};
 
-  ASSERT_EQ(validTask.GetName(), dto.getName());
-  ASSERT_EQ(validTask.GetLabel(), dto.getLabel());
-  ASSERT_EQ(validTask.GetPriority(), dto.getPriority());
-  ASSERT_EQ(validTask.GetDate().GetDate(), dto.getDueDate());
-  ASSERT_EQ(5, dto.getID());
+  TaskDTO dto;
+  EXPECT_NO_THROW(dto = TaskDTO::create(id, "name", "label", Priority::MEDIUM, date, false));
+
+  ASSERT_EQ("name", dto.getName());
+  ASSERT_EQ("label", dto.getLabel());
+  ASSERT_EQ(Priority::MEDIUM, dto.getPriority());
+  ASSERT_EQ(date, dto.getDueDate());
+  ASSERT_EQ(id, dto.getID());
   ASSERT_EQ(false, dto.getStatus());
 }
 
---
+++ tests/core/MemoryModel/Storage/ModelTaskDTOTest.cpp
@@ -0,0 +1,33 @@
+//
+// Created by rodion on 10/6/20.
+//
+
+#include <src/core/MemoryModel/Storage/ModelTaskDTO.h>
+#include <gtest/gtest.h>
+#include <src/core/MemoryModel/Task/Task.h>
+
+class ModelTaskDTOTest : public ::testing::Test {
+  void SetUp() override {
+
+  }
+
+  void TearDown() override {
+
+  }
+};
+
+TEST_F(ModelTaskDTOTest, validTask) {
+  Task task = Task::create("name", "label", Priority::MEDIUM, Date{2020, 10, 6}).value();
+
+  ModelTaskDTO dto;
+  EXPECT_NO_THROW(dto = ModelTaskDTO::createWithParent(
+      TaskID{5}, task.GetName(), task.GetLabel(), task.GetPriority(), task.GetDate(), false, TaskID{1}));
+
+  ASSERT_EQ(task.GetName(), dto.getName());
+  ASSERT_EQ(task.GetLabel(), dto.getLabel());
+  ASSERT_EQ(task.GetPriority(), dto.getPriority());
+  ASSERT_EQ(task.GetDate().GetDate(), dto.getDueDate().GetDate());
+  ASSERT_EQ(TaskID{5}, dto.getID());
+  ASSERT_EQ(TaskID{1}, dto.getParentID());
+  ASSERT_EQ(false, dto.getStatus());
+}
